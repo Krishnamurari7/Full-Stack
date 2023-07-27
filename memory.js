5786437f95ea02e4057a54348018fed74945c8bc
Krishna murari kumar <139735281+Krishnamurari7@users.noreply.github.com>
2023-07-28 01:19:32 +0530

    Add files via upload

---
+++ memory.js
@@ -0,0 +1,7 @@
+//stack(primitive) ,Heap(Non-primitive)
+let myyoutubename = "KMcoder"
+let anothername = myyoutubename
+console.log(anothername)
+anothername = "gang dark fighters"
+console.log(myyoutubename)
+console.log(anothername)
