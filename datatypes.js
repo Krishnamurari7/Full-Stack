5ada5128acc506503cadffebd8e1de089d2fdf15
Krishnamurari7 <139735281+Krishnamurari7@users.noreply.github.com>
2023-07-28 00:04:07 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,27 @@
+// number, null, string,symbol,boolean,bigint,undefined
+const score = 100
+console.log(typeof score)
+const id = true
+console.log(typeof id)
+const namee = "krishna"
+console.log(typeof namee)
+const out = null
+console.log(typeof out)
+const sym = Symbol('123')
+console.log(typeof sym)
+console.log( id===sym)
+const an =
+console.log(typeof and)
+const bignumber = 59656758466235778n
+console.log(typeof bignumber)
+const heros = ["shaktiman","naagraj","doga","kaliya"]
+console.log(heros)
+let myobj ={
+    name:"krishna",
+    age:21,
+}
+console.log(myobj)
+const myfunction = function(){
+    console.log("hello world")
+}
+console.log(myfunction)
