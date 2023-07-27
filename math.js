d872c38c98c38cca62b5061908fbc8d919f9e0cb
Krishnamurari7 <139735281+Krishnamurari7@users.noreply.github.com>
2023-07-28 00:05:43 +0530

    Add files via upload

---
+++ math.js
@@ -0,0 +1,17 @@
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.4));
+console.log(Math.ceil(4.5));
+console.log(Math.floor(4.5));
+console.log(Math.min(4,5,23,45,32,43));
+console.log(Math.max(4,5,23,45,32,43));
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.random()*10+1);
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 200
+Math.random() * (max - min +1)
+console.log(Math.random() * (max - min +1))
