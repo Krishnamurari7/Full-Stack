1daa9dcf418a046c0213ccf5df6ec33a21d24ccd
Krishnamurari7 <139735281+Krishnamurari7@users.noreply.github.com>
2023-07-28 00:04:26 +0530

    Add files via upload

---
+++ date.js
@@ -0,0 +1,12 @@
+const mydate = new Date()
+console.log(mydate);
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toTimeString());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toLocaleString());
+console.log(typeof mydate);
+
+let mycreate = new Date("2023-01-30")
+console.log(mycreate);
+console.log(mycreate.toLocaleString());
