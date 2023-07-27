5acf2f5deac2dbfee336d9dd81d3ea86c7751eb5
Krishnamurari7 <139735281+Krishnamurari7@users.noreply.github.com>
2023-07-28 00:14:39 +0530

    Add files via upload

---
+++ number.js
@@ -0,0 +1,19 @@
+const score = 500
+console.log(score)
+
+const balance = new Number(300)
+console.log(balance)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const othernumber = 34.97676
+console.log(othernumber.toPrecision(3))
+
+const hun = 1000000
+//us level
+console.log(hun.toLocaleString())
+//indian level
+console.log(hun.toLocaleString('en-IN'))
+
+
