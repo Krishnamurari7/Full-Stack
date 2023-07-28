3dc6c6c3d877ade063edeb7e5bc7090760d9974a
Krishna murari kumar <139735281+Krishnamurari7@users.noreply.github.com>
2023-07-28 22:34:29 +0530

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,22 @@
+const myArr = [0, 1, 2, 3, 4, 5]
+console.log(myArr[0]);
+console.log(myArr);
+const numbers = [1, 2, 3, 4,]
+console.log(numbers[0]);
+//Array Methods
+myArr.push(6)
+myArr.push(7)
+console.log(myArr);
+myArr.pop()
+myArr.pop()
+myArr.pop()
+myArr.pop()
+console.log(myArr);
+myArr.unshift(9)
+console.log(myArr);
+myArr.shift()
+console.log(myArr);
+const newarr = myArr.join()
+console.log(myArr);
+console.log(newarr);
+console.log(typeof newarr);
