91b75a4a965c64a9c17340046518a1d653c36c30
Krishna murari kumar <139735281+Krishnamurari7@users.noreply.github.com>
2023-07-29 23:49:47 +0530

    Update variable.js

--- variable.js
+++ variable.js
@@ -4,6 +4,9 @@ ab = "krishna"
 console.log(ab);
 // let 6dkh = 76   sentax error
 // let var = "krish"  sentax error
+const author = "murari"
+// const author = "isu"   const is constant value constant can not be change
+console.log(author);
 let a = 76
 let b = "krishna"
 let c = null
