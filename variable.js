d8572a5495d0cf564cb56c7f47bf57f6ffaf86a0
Krishna murari kumar <139735281+Krishnamurari7@users.noreply.github.com>
2023-07-29 23:45:19 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,15 @@
+let ab = 76
+console.log(ab);
+ab = "krishna"
+console.log(ab);
+// let 6dkh = 76   sentax error
+// let var = "krish"  sentax error
+let a = 76
+let b = "krishna"
+let c = null
+let d = undefined
+{
+    let b = 'this'
+    console.log(b)
+}
+console.log(b);
