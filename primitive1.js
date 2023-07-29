ce2b561b0f0888ca7c9c6dfc9653e63184c9d4a8
Krishna murari kumar <139735281+Krishnamurari7@users.noreply.github.com>
2023-07-29 23:59:22 +0530

    Add files via upload

---
+++ primitive1.js
@@ -0,0 +1,17 @@
+let a = null
+let b = 345
+let c = true
+let d = BigInt("765")
+let e = "krishna"
+let f = Symbol("ham achhe hai!")
+let g = undefined
+let h
+console.log(a, b, c, d, e, f, g, h);
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+console.log(typeof g);
+console.log(typeof h);
