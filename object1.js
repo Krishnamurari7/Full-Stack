1b008cc1b8a0fecc291fa77b836607c7551fa520
Krishna murari kumar <139735281+Krishnamurari7@users.noreply.github.com>
2023-07-29 16:37:01 +0530

    Add files via upload

---
+++ object1.js
@@ -0,0 +1,15 @@
+const home = new Object()
+// const home = {}
+home.id = "123abc"
+home.name = "krishna"
+home.isLoggedIn = false
+// console.log(home);
+const regularUser = {
+    fullName : {
+        userFullname:{
+            firstname: "krishna",
+            lastname: "murari"
+        }
+    }
+}
+console.log(regularUser.fullName.userFullname);
