1153c7538e397905747d31d1793d7e62e9a18f1a
Krishna murari kumar <139735281+Krishnamurari7@users.noreply.github.com>
2023-07-30 00:31:23 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,28 @@
+console.log("operators in javascript");
+let a = 65
+let b = 4
+console.log("a + b = ",a+b);
+console.log("a - b = ",a-b);
+console.log("a * b = ",a*b);
+console.log("a / b = ",a/b);
+console.log("a % b = ",a%b);
+console.log("a ** b = ",a**b);
+console.log("a++ = ",a++);
+console.log("a-- = ",a--);
+console.log("++a = ",++a);
+console.log("--a = ",--a);
+
+
+let comp1 = 6
+let comp2 = 7
+console.log("comp1 == comp2 is ",comp1 == comp2);
+console.log("comp1 != comp2 is ",comp1 != comp2);
+console.log("comp1 === comp2 is ",comp1 === comp2);
+console.log("comp1 !== comp2 is ",comp1 !== comp2);
+console.log("comp1 > comp2 is ",comp1 > comp2);
+
+
+let x = 5
+let y = 6
+console.log(x<y && x==y); // condition right is true
+console.log(x<y || x==y);//ek right is true
