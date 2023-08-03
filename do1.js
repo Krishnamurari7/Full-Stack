40ddb8c7288dae28b0e5d8f7de300fd10cc988ea
Krishna murari kumar <139735281+Krishnamurari7@users.noreply.github.com>
2023-08-03 19:58:57 +0530

    Add files via upload

---
+++ do1.js
@@ -0,0 +1,15 @@
+let arr = 0;
+let myarray = ['flash', 'krishna', 'vishnu']
+while ( arr<myarray.length) {
+    console.log(`name is ${myarray[arr]}`);
+    arr++
+}
+
+
+let score = 1
+do{
+    console.log(`score is ${score}`);
+    score++
+}while (score <= 10) {
+    
+}
