ff49a1936e1d00fbe3cb12d1e0209a8d7edf7903
Krishna murari kumar <139735281+Krishnamurari7@users.noreply.github.com>
2023-08-06 18:46:48 +0530

    Add files via upload

---
+++ invoked.js
@@ -0,0 +1,14 @@
+(function chai(){
+    console.log(`DB CONNECTED`);
+})();
+
+
+(() => {
+    console.log(`DB CONNECTED2 one`);
+})();
+
+
+
+((name) => {
+    console.log(`DB CONNECTED2 ${name}`);
+})(`krishna`)
