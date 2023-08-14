c564ea7284122717c2c1edb70052cb97297540ac
Krishna murari kumar <139735281+Krishnamurari7@users.noreply.github.com>
2023-08-14 16:30:22 +0530

    Add files via upload

---
+++ control_flow_1.js
@@ -0,0 +1,35 @@
+if(2 == 2){
+    console.log("i am krishna");
+}
+console.log("sonu");
+
+
+if(2 === 2){
+    console.log("i am krishna");
+}
+console.log("sonu");
+
+
+if(2 == "2"){
+    console.log("krishna");
+}
+console.log("sonu");
+
+
+if(2 === "2"){
+    console.log("krishna");
+}
+console.log("sonu");
+
+
+const score = 200
+
+if(score > 100){
+    const power = "fly"
+    console.log(`user power: ${power}`);
+}
+
+
+const balance = 1000
+if(balance > 500)console.log("test"),console.log("exam");
+
