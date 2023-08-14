761bfdbf3288067c698934baf27ea73a766fd60c
Krishna murari kumar <139735281+Krishnamurari7@users.noreply.github.com>
2023-08-14 16:43:15 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,32 @@
+const month = 3
+  
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        break;
+    case 7:
+        console.log("july");
+        break;
+    case 8:
+        console.log("August");
+        break;
+
+    default:
+        console.log("default case executed");
+        break;
+}
